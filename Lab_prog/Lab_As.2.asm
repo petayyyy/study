556e07fcc072c018b37d6f96037e88e477923c9e
petayyyy <47917455+petayyyy@users.noreply.github.com>
2021-01-13 13:27:41 +0300

    Update Lab_As.2.asm

--- Lab_prog/Lab_As.2.asm
+++ Lab_prog/Lab_As.2.asm
@@ -9,15 +9,15 @@
 .def REG1 = r18
 .def REG2 = r19
 .def RESULT = r20
-.def TEMP = r16
+.def STAK = r16
 .def COUNT = r17
 .DEF DEBUG = r0
 
-ldi TEMP, low(RAMEND)
-out SPL, TEMP
-ldi TEMP, high(RAMEND)
-out SPH, TEMP
-clr TEMP
+ldi STAK, low(RAMEND)
+out SPL, STAK
+ldi STAK, high(RAMEND)
+out SPH, STAK
+clr STAK
 
 .cseg
 .org 0x07
@@ -25,46 +25,56 @@ clr TEMP
 LDI REG1, B				; REG1 = B, REG2 = 0, RESULT = 0 ; COUNT = 0 ; DEBUG = 0; STACK: -
 LDI REG2, 4				; REG1 = B, REG2 = 4, RESULT = 0 ; COUNT = 0 ; DEBUG = 0; STACK: -
 ADD REG1, REG2			; REG1 = B+4, REG2 = 4, RESULT = 0 ; COUNT = 0 ; DEBUG = 0; STACK: -
+PUSH REG1				; REG1 = B+4, REG2 = 4, RESULT = 0 ; COUNT = 0 ; DEBUG = 0; STACK: B+4
 BRBS 0, error
 
-LDI REG2, A				; REG1 = B+4, REG2 = A, RESULT = 0 ; COUNT = 0 ; DEBUG = 0; STACK: -
-MUL REG1,REG2			; REG1 = B+4, REG2 = A, RESULT = 0 ; COUNT = 0 ; DEBUG = (B+4) * A; STACK: -
-MOV REG1,DEBUG			; REG1 = (B+4) * A, REG2 = A, RESULT = 0 ; COUNT = 0 ; DEBUG = (B+4) * A; STACK: -
+LDI REG2, A				; REG1 = B+4, REG2 = A, RESULT = 0 ; COUNT = 0 ; DEBUG = 0; STACK: B+4
+RCALL MULTIPLICATE		; REG1 = (B+4) * A, REG2 = A, RESULT = 0 ; COUNT = 0 ; DEBUG = (B+4) * A; STACK: B+4
+PUSH REG1				; REG1 = (B+4) * A, REG2 = A, RESULT = 0 ; COUNT = 0 ; DEBUG = (B+4) * A; STACK: B+4, (B+4) * A
+BRBS 0, ERROR
 
-LDI REG2, 5				; REG1 = (B+4) * A, REG2 = 5, RESULT = 0 ; COUNT = 0 ; DEBUG = (B+4) * A;STACK: -
-RCALL DELETE			; REG1 = ((B+4) * A)/5, REG2 = 5, RESULT = 0 ; COUNT = 0 ; DEBUG = (B+4) * A;STACK: -
-PUSH REG1				; REG1 = ((B+4) * A)/5, REG2 = 5, RESULT = 0 ; COUNT = 0 ; DEBUG = (B+4) * A;STACK: ((B+4) * A)/5
+LDI REG2, 5				; REG1 = (B+4) * A, REG2 = 5, RESULT = 0 ; COUNT = 0 ; DEBUG = (B+4) * A; STACK: B+4, (B+4) * A
+RCALL DELETE			; REG1 = ((B+4) * A)/5, REG2 = 5, RESULT = 0 ; COUNT = 0 ; DEBUG = (B+4) * A; STACK: B+4, (B+4) * A
+PUSH REG1				; REG1 = ((B+4) * A)/5, REG2 = 5, RESULT = 0 ; COUNT = 0 ; DEBUG = (B+4) * A; STACK: B+4, (B+4) * A, ((B+4) * A)/5
 
-LDI REG1, C				; REG1 = C, REG2 = 5, RESULT = 0 ; COUNT = 0 ; DEBUG = (B+4) * A;STACK: ((B+4) * A)/5
-LDI REG2, 1				; REG1 = C, REG2 = 1, RESULT = 0 ; COUNT = 0 ; DEBUG = (B+4) * A;STACK: ((B+4) * A)/5
-SUB REG1, REG2			; REG1 = C-1, REG2 = 1, RESULT = 0 ; COUNT = 0 ; DEBUG = (B+4) * A;STACK: ((B+4) * A)/5
-PUSH REG1				; REG1 = C-1, REG2 = 1, RESULT = 0 ; COUNT = 0 ; DEBUG = (B+4) * A;STACK: ((B+4) * A)/5, C-1
-BRBS 2, error
+LDI REG1, C				; REG1 = C, REG2 = 5, RESULT = 0 ; COUNT = 0 ; DEBUG = (B+4) * A; STACK: B+4, (B+4) * A, ((B+4) * A)/5
+LDI REG2, 1				; REG1 = C, REG2 = 1, RESULT = 0 ; COUNT = 0 ; DEBUG = (B+4) * A; STACK: B+4, (B+4) * A, ((B+4) * A)/5
+SUB REG1, REG2			; REG1 = C-1, REG2 = 1, RESULT = 0 ; COUNT = 0 ; DEBUG = (B+4) * A; STACK: B+4, (B+4) * A, ((B+4) * A)/5
+PUSH REG1				; REG1 = C-1, REG2 = 1, RESULT = 0 ; COUNT = 0 ; DEBUG = (B+4) * A; STACK: B+4, (B+4) * A, ((B+4) * A)/5, C-1
+BRBS 2, ERROR
 
-LDI REG1, D				; REG1 = D, REG2 = 1, RESULT = 0 ; COUNT = 0 ; DEBUG = (B+4) * A;STACK: ((B+4) * A)/5, C-1
-LDI REG2, 2				; REG1 = D, REG2 = 2, RESULT = 0 ; COUNT = 0 ; DEBUG = (B+4) * A;STACK: ((B+4) * A)/5, C-1
-ADD REG1, REG2			; REG1 = D+2, REG2 = 2, RESULT = 0 ; COUNT = 0 ; DEBUG = (B+4) * A;STACK: ((B+4) * A)/5, C-1
+LDI REG1, D				; REG1 = D, REG2 = 1, RESULT = 0 ; COUNT = 0 ; DEBUG = (B+4) * A; STACK: B+4, (B+4) * A, ((B+4) * A)/5, C-1
+LDI REG2, 2				; REG1 = D, REG2 = 2, RESULT = 0 ; COUNT = 0 ; DEBUG = (B+4) * A; STACK: B+4, (B+4) * A, ((B+4) * A)/5, C-1
+ADD REG1, REG2			; REG1 = D+2, REG2 = 2, RESULT = 0 ; COUNT = 0 ; DEBUG = (B+4) * A; STACK: B+4, (B+4) * A, ((B+4) * A)/5, C-1
+BRBS 0, ERROR
 
-POP REG2				; REG1 = D+2, REG2 = C-1, RESULT = 0 ; COUNT = 0 ; DEBUG = (B+4) * A;STACK: ((B+4) * A)/5
-MUL REG1,REG2			; REG1 = D+2, REG2 = C-1, RESULT = 0 ; COUNT = 0 ;DEBUG = (D+2) * (C-1); STACK: ((B+4) * A)/5
-MOV REG1,DEBUG			; REG1 = (D+2) * (C-1), REG2 = C-1, RESULT = 0 ; COUNT = 0 ;DEBUG = (D+2) * (C-1); STACK: ((B+4) * A)/5
+POP REG2				; REG1 = D+2, REG2 = C-1, RESULT = 0 ; COUNT = 0 ; DEBUG = (B+4) * A; STACK: B+4, (B+4) * A, ((B+4) * A)/5
+RCALL MULTIPLICATE		; REG1 = (D+2) * (C-1), REG2 = C-1, RESULT = 0 ; COUNT = 0 ;DEBUG = (D+2) * (C-1); STACK: ((B+4) * A)/5
+BRBS 0, ERROR
 
-LDI REG2, 2				; REG1 = (D+2) * (C-1), REG2 = 2, RESULT = 0 ; COUNT = 0 ;DEBUG = (D+2) * (C-1); STACK: ((B+4) * A)/5
-RCALL DELETE			; REG1 = (D+2) * (C-1)/2, REG2 = 2, RESULT = 0 ; COUNT = 0 ; DEBUG = (B+4) * A;STACK: ((B+4) * A)/5
+LDI REG2, 2				; REG1 = (D+2) * (C-1), REG2 = 2, RESULT = 0 ; COUNT = 0 ;DEBUG = (D+2) * (C-1); STACK: B+4, (B+4) * A, ((B+4) * A)/5
+RCALL DELETE			; REG1 = (D+2) * (C-1)/2, REG2 = 2, RESULT = 0 ; COUNT = 0 ; DEBUG = (B+4) * A; STACK: B+4, (B+4) * A, ((B+4) * A)/5
 
-POP REG2				; REG1 = (D+2)*(C-1)/2, REG2 = ((B+4) * A)/5, RESULT = 0 ; COUNT = 0 ; DEBUG = (B+4) * A; STACK: -
-ADD REG1, REG2			; REG1 = (D+2)*(C-1)/2 + ((B+4) * A)/5, REG2 = ((B+4) * A)/5, RESULT = 0 ; COUNT = 0 ; DEBUG = (B+4) * A; STACK: -
-BRBS 0, error
+POP REG2				; REG1 = (D+2)*(C-1)/2, REG2 = ((B+4) * A)/5, RESULT = 0 ; COUNT = 0 ; DEBUG = (B+4) * A; STACK: STACK: B+4, (B+4) * A
+ADD REG1, REG2			; REG1 = (D+2)*(C-1)/2 + ((B+4) * A)/5, REG2 = ((B+4) * A)/5, RESULT = 0 ; COUNT = 0 ; DEBUG = (B+4) * A; STACK: STACK: B+4, (B+4) * A
+BRBS 0, ERROR
 
-MOV RESULT, REG1		; REG1 = (D+2)*(C-1)/2 + ((B+4) * A)/5, REG2 = ((B+4) * A)/5, RESULT = (D+2)*(C-1)/2 + ((B+4) * A)/5 ; COUNT = 0 ; DEBUG =  (B+4) * A; STACK: -
-CLR REG1				; REG1 = 0 , REG2 = ((B+4) * A)/5, RESULT = (D+2)*(C-1)/2 + ((B+4) * A)/5 ; COUNT = 0 ; DEBUG = (B+4) * A; STACK: -
-CLR REG2				; REG1 = 0 , REG2 = 0 , RESULT = (D+2)*(C-1)/2 + ((B+4) * A)/5 ; COUNT = 0 ; DEBUG = (B+4) * A; STACK: -
-CLR DEBUG				; REG1 = 0 , REG2 = 0 , RESULT = (D+2)*(C-1)/2 + ((B+4) * A)/5 ; COUNT = 0 ; DEBUG = 0; STACK: -
+MOV RESULT, REG1		; REG1 = (D+2)*(C-1)/2 + ((B+4) * A)/5, REG2 = ((B+4) * A)/5, RESULT = (D+2)*(C-1)/2 + ((B+4) * A)/5 ; COUNT = 0 ; DEBUG =  (B+4) * A; STACK: STACK: B+4, (B+4) * A
+CLR REG1				; REG1 = 0 , REG2 = ((B+4) * A)/5, RESULT = (D+2)*(C-1)/2 + ((B+4) * A)/5 ; COUNT = 0 ; DEBUG = (B+4) * A; STACK: STACK: B+4, (B+4) * A
+CLR REG2				; REG1 = 0 , REG2 = 0 , RESULT = (D+2)*(C-1)/2 + ((B+4) * A)/5 ; COUNT = 0 ; DEBUG = (B+4) * A; STACK: STACK: B+4, (B+4) * A
+CLR DEBUG				; REG1 = 0 , REG2 = 0 , RESULT = (D+2)*(C-1)/2 + ((B+4) * A)/5 ; COUNT = 0 ; DEBUG = 0; STACK: STACK: B+4, (B+4) * A
 RCALL LOOP
 
 OUT PORTD, RESULT
-LOOP: RJMP LOOP
+LOOP: 
+OUT PORTD, RESULT
+RJMP LOOP
 ERROR: RJMP ERROR
+
+MULTIPLICATE:
+MUL REG1,REG2				
+MOV REG1,DEBUG
+RET
 
 DELETE:
 CP REG1, REG2

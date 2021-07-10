374a08e759acca471a571eb454a001b3eaed8eb4
petayyyy <47917455+petayyyy@users.noreply.github.com>
2021-07-11 02:09:45 +0300

    Update ProductData.cs

--- Lab_prog/2Sem/lab_4/Lab4/ProductData.cs
+++ Lab_prog/2Sem/lab_4/Lab4/ProductData.cs
@@ -11,123 +11,140 @@ using System.Windows.Forms;
 
 namespace Lab4
 {
-    public partial class ProductData : Form
+    public partial class ChangeCipher : Form
     {
         public static string connectString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=DataBaseLab4.mdb;";
         private OleDbConnection myConnection;
-        private int min;
-        private int max;
-        private bool er1 = false;
-        private bool er2 = false;
-        public ProductData()
+        public ChangeCipher()
         {
             InitializeComponent();
 
             myConnection = new OleDbConnection(connectString);
         }
-
-        private void ProductData_Load(object sender, EventArgs e)
+        private int idcom = -1;
+        private void CompanyComboBox_SelectedIndexChanged(object sender, EventArgs e)
         {
+            string com = CompanyComboBox.SelectedItem.ToString();
+            idcom = -1;
 
+            myConnection.Open();
+
+            OleDbCommand myOleDbCommand = myConnection.CreateCommand();
+            myOleDbCommand.CommandText = "SELECT [Район] FROM [Место] WHERE [Код района] = '" + com + "'";
+            OleDbDataReader myOleDbDataReader = myOleDbCommand.ExecuteReader();
+            while (myOleDbDataReader.Read())
+            {
+                idcom = Convert.ToInt32(myOleDbDataReader[0]);
+            }
+            myOleDbDataReader.Close();
+
+            dataGridView1.ColumnCount = 1;
+            for (int k = 0; k < dataGridView1.ColumnCount; k++)
+                dataGridView1.Columns[k].SortMode = DataGridViewColumnSortMode.NotSortable;
+            dataGridView1.RowHeadersVisible = false;
+
+            dataGridView1.Columns[0].HeaderText = "Номер школы";
+            dataGridView1.Columns[0].Width = 300;
+            dataGridView1.Width = 400;
+
+            myOleDbCommand.CommandText = "SELECT [Номер школы] FROM [Ведомость] WHERE [Код района] = " + idcom.ToString();
+            OleDbDataReader myOleDbDataReader1 = myOleDbCommand.ExecuteReader();
+            dataGridView1.RowCount = 1;
+            int i = 0;
+            while (myOleDbDataReader1.Read())
+            {
+                dataGridView1.RowCount += 1;
+                dataGridView1.Rows[i].Cells[0].Value = myOleDbDataReader1[0];
+                i++;
+            }
+            myOleDbDataReader1.Close();
+
+            myConnection.Close();
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void ChangeCipher_Load(object sender, EventArgs e)
         {
-            if (MinTextBox.Text != "")
+            myConnection.Open();
+
+            CompanyComboBox.Items.Clear();
+            CompanyComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+
+            OleDbCommand myOleDbCommand = myConnection.CreateCommand();
+            myOleDbCommand.CommandText = "SELECT * FROM [Место] WHERE [Код района]";
+            OleDbDataReader myOleDbDataReader = myOleDbCommand.ExecuteReader();
+            while (myOleDbDataReader.Read())
             {
-                bool res = int.TryParse(MinTextBox.Text, out min);
-                if (res != true)
+                CompanyComboBox.Items.Add(myOleDbDataReader[1]);
+            }
+            myOleDbDataReader.Close();
+
+            myConnection.Close();
+        }
+        private string pred = "";
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                if (dataGridView1.RowCount != 1 && e.RowIndex != dataGridView1.RowCount - 1)
                 {
-                    MessageBox.Show("Введено не число или не целое");
-                    MinTextBox.Text = MinTextBox.Text.Substring(0, MinTextBox.Text.Length - 1);
-                    FoundButton.Enabled = false;
-                    er1 = false;
-                }
-                else
-                {
-                    if (min < 0)
-                    {
-                        MessageBox.Show("Введено отрицательное число");
-                        MinTextBox.Text = "";
-                        FoundButton.Enabled = false;
-                        er1 = false;
-                    }
-                    er1 = true;
-                    if (er2 != false)
-                        FoundButton.Enabled = true;
+                    NewNameTextBox.Text = dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString().Trim();
+                    pred = NewNameTextBox.Text;
+                    NewNameTextBox.Enabled = true;
+                    ChangeButton.Enabled = true;
+                    label3.Visible = true;
                 }
             }
         }
 
-        private void MaxTextBox_TextChanged(object sender, EventArgs e)
+        private void ChangeButton_Click(object sender, EventArgs e)
         {
-            if (MaxTextBox.Text != "")
-            {
-                bool res = int.TryParse(MaxTextBox.Text, out max);
-                if (res != true)
-                {
-                    MessageBox.Show("Введено не число или не целое");
-                    MaxTextBox.Text = MaxTextBox.Text.Substring(0, MaxTextBox.Text.Length - 1);
-                    FoundButton.Enabled = false;
-                    er2 = false;
-                }
-                else
-                {
-                    er2 = true;
-                    if (er1 != false)
-                        FoundButton.Enabled = true;
-                }
-            }
-        }
+            string newname = NewNameTextBox.Text;
 
-        private void FoundButton_Click(object sender, EventArgs e)
-        {
-            if (max < min)
+            if (pred == newname || newname.Trim() == "")
             {
-                MessageBox.Show("Максимальное не может быть меньше минимального");
-                FoundButton.Enabled = false;
+                if (pred == newname)
+                    MessageBox.Show("Вы не ввели новый шифр");
+                else
+                    MessageBox.Show("Вы ничего не ввели или одни пробелы");
             }
             else
             {
+                int er = 0;
+                NewNameTextBox.Enabled = false;
+                NewNameTextBox.Text = "";
+                ChangeButton.Enabled = false;
+                label3.Visible = false;
+
                 myConnection.Open();
 
-                dataGridView1.ColumnCount = 7;
-                for (int k = 0; k < dataGridView1.ColumnCount; k++)
-                    dataGridView1.Columns[k].SortMode = DataGridViewColumnSortMode.NotSortable;
-                dataGridView1.RowHeadersVisible = false;
-
-                dataGridView1.Columns[0].HeaderText = "Код района";
-                dataGridView1.Columns[0].Width = 40;
-                dataGridView1.Columns[1].HeaderText = "Номер школы";
-                dataGridView1.Columns[1].Width = 100;
-                dataGridView1.Columns[2].HeaderText = "Телефон";
-                dataGridView1.Columns[2].Width = 100;
-                dataGridView1.Columns[3].HeaderText = "Год открытия";
-                dataGridView1.Columns[3].Width = 100;
-                dataGridView1.Columns[4].HeaderText = "Количество учителей";
-                dataGridView1.Columns[4].Width = 100;
-                dataGridView1.Columns[5].HeaderText = "Количество учеников";
-                dataGridView1.Columns[5].Width = 100;
-                dataGridView1.Width = 660;
-
                 OleDbCommand myOleDbCommand = myConnection.CreateCommand();
-                myOleDbCommand.CommandText = "SELECT * FROM [Изделия] WHERE ([Телефон] <= [Год открытия] AND [Год открытия] <= [Количество учителей] AND [Количество учителей] <= [Количество учеников] AND ([Количество учеников] * [Средняя цена единицы за год]) >=" + min.ToString() + " AND ([Количество учеников] * [Средняя цена единицы за год]) <=" + max.ToString() + ")";
+
+                myOleDbCommand.CommandText = "SELECT [Номер школы] FROM [Ведомость] WHERE [Номер школы] = '" + newname + "'";
                 OleDbDataReader myOleDbDataReader1 = myOleDbCommand.ExecuteReader();
-                int i = 0;
-                dataGridView1.RowCount = 1;
                 while (myOleDbDataReader1.Read())
                 {
-                    dataGridView1.RowCount += 1;
-                    dataGridView1.Rows[i].Cells[0].Value = myOleDbDataReader1["Код района"];
-                    dataGridView1.Rows[i].Cells[1].Value = myOleDbDataReader1["Номер школы"].ToString();
-                    dataGridView1.Rows[i].Cells[2].Value = myOleDbDataReader1["Телефон"];
-                    dataGridView1.Rows[i].Cells[3].Value = myOleDbDataReader1["Год открытия"];
-                    dataGridView1.Rows[i].Cells[4].Value = myOleDbDataReader1["Количество учителей"];
-                    dataGridView1.Rows[i].Cells[5].Value = myOleDbDataReader1["Количество учеников"];
-                    i++;
+                    MessageBox.Show("'" + myOleDbDataReader1[0].ToString() + "' уже существует");
+                    er = 1;
                 }
                 myOleDbDataReader1.Close();
 
+                if (er != 1)
+                {
+                    myOleDbCommand.CommandText = "UPDATE [Ведомость] SET [Номер школы] = '" + newname + "' WHERE [Номер школы] = '" + pred + "'";
+                    myOleDbCommand.ExecuteNonQuery();
+
+                    myOleDbCommand.CommandText = "SELECT [Номер школы] FROM [Ведомость] WHERE [Район] = " + idcom.ToString();
+                    OleDbDataReader myOleDbDataReader = myOleDbCommand.ExecuteReader();
+                    dataGridView1.RowCount = 1;
+                    int i = 0;
+                    while (myOleDbDataReader.Read())
+                    {
+                        dataGridView1.RowCount += 1;
+                        dataGridView1.Rows[i].Cells[0].Value = myOleDbDataReader[0];
+                        i++;
+                    }
+                    myOleDbDataReader.Close();
+                }
                 myConnection.Close();
             }
         }

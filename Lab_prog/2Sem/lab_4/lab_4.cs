4b618ae6b338aae3e27a69b019c43d05f332121d
petayyyy <47917455+petayyyy@users.noreply.github.com>
2021-07-10 20:20:16 +0300

    Create lab_4.cs

---
+++ Lab_prog/2Sem/lab_4/lab_4.cs
@@ -0,0 +1,25 @@
+
+Microsoft Visual Studio Solution File, Format Version 12.00
+# Visual Studio Version 16
+VisualStudioVersion = 16.0.30011.22
+MinimumVisualStudioVersion = 10.0.40219.1
+Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "Lab4", "Lab4\Lab4.csproj", "{399689AE-9BA7-4E9C-8EB7-7CBE914C2B3C}"
+EndProject
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Debug|Any CPU = Debug|Any CPU
+		Release|Any CPU = Release|Any CPU
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{399689AE-9BA7-4E9C-8EB7-7CBE914C2B3C}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
+		{399689AE-9BA7-4E9C-8EB7-7CBE914C2B3C}.Debug|Any CPU.Build.0 = Debug|Any CPU
+		{399689AE-9BA7-4E9C-8EB7-7CBE914C2B3C}.Release|Any CPU.ActiveCfg = Release|Any CPU
+		{399689AE-9BA7-4E9C-8EB7-7CBE914C2B3C}.Release|Any CPU.Build.0 = Release|Any CPU
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {20426E4C-AEE4-45E2-ABD9-1716EAF733AC}
+	EndGlobalSection
+EndGlobal
